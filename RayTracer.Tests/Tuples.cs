fc9524b5a080445701713e76de0925c36a216202
Ryan Hagan <ryan@ryanhagan.net>
2019-01-13 19:47:01 -0500

    All basic functionality created for Points and Vectors.

--- RayTracer.Tests/Tuples.cs
+++ RayTracer.Tests/Tuples.cs
@@ -76,5 +76,161 @@ namespace RayTracer.Tests
             Assert.True(firstVector.Equals(secondVector));
         }
 
+        [Fact]
+        public void AddingPointAndVectors_ShouldCreateNewPoint()
+        {
+            var myPoint = new Point(3.0, -2.0, 5.0);
+            var myVector = new Vector(-2.0, 3.0, 1.0);
+            RayTuple result = myPoint + myVector;
+            Assert.True(result.Equals(new Point(1.0, 1.0, 6.0)));
+        }
+
+        [Fact]
+        public void AddingTwoVectors_ShouldCreateNewVector()
+        {
+            var myVector1 = new Vector(3.0, -2.0, 5.0);
+            var myVector2 = new Vector(-2.0, 3.0, 1.0);
+            RayTuple result = myVector1 + myVector2;
+            Assert.True(result.Equals(new Vector(1.0, 1.0, 6.0)));
+        }
+
+        [Fact]
+        public void SubtractingTwoPoints_ShouldCreateNewVector()
+        {
+            var myPoint1 = new Point(3, 2, 1);
+            var myPoint2 = new Point(5, 6, 7);
+            RayTuple result = myPoint1 - myPoint2;
+            Assert.True(result.Equals(new Vector(-2, -4, -6)));
+        }
+
+        [Fact]
+        public void SubtractingVectorFromPoint_ShouldCreateNewPoint()
+        {
+            var myPoint = new Point(1, 2, 3);
+            var myVector = new Vector(4, 5, 6);
+            RayTuple result = myPoint - myVector;
+            Assert.True(result.Equals(new Point(-3, -3, -3)));
+        }
+
+        [Fact]
+        public void SubtractingTwoVectors_ShouldCreateNewVector()
+        {
+            var myVector1 = new Vector(4, 5, 6);
+            var myVector2 = new Vector(1, 2, 3);
+            RayTuple result = myVector1 - myVector2;
+            Assert.True(result.Equals(new Vector(3, 3, 3)));
+        }
+
+        [Fact]
+        public void SubtractingPointFromVector_ShouldFail()
+        {
+            var myVector = new Vector(4, 5, 6);
+            var myPoint = new Point(1, 2, 3);
+            Exception ex = Assert.Throws<InvalidOperationException>(() => myVector - myPoint);
+        }
+
+        [Fact]
+        public void SubtractingVectorFromZeroVector_ShouldNegateVector()
+        {
+            var zeroVector = new Vector(0, 0, 0);
+            var myVector = new Vector(1, -2, 3);
+            RayTuple result = zeroVector - myVector;
+            Assert.True(result.Equals(new Vector(-1, 2, -3)));
+        }
+
+        [Fact]
+        public void NegatingTuple_ShouldWork()
+        {
+            var myVector = new RayTuple(1, -2, 3, -4);
+            RayTuple result = -myVector;
+            Assert.True(result.Equals(new RayTuple(-1, 2, -3, 4)));
+        }
+
+        [Fact]
+        public void MultiplingTupleByScalar_ShouldMultiplyEachElementByScalar()
+        {
+            var myTuple = new RayTuple(1, -2, 3, -4);
+            RayTuple result = myTuple * 3.5;
+            Assert.True(result.Equals(new RayTuple(3.5, -7.0, 10.5, -14)));
+        }
+
+        [Fact]
+        public void MultiplingTupleByFraction_ShouldMultiplyEachElementByFraction()
+        {
+            var myTuple = new RayTuple(1, -2, 3, -4);
+            RayTuple result = myTuple * 0.5;
+            Assert.True(result.Equals(new RayTuple(0.5, -1.0, 1.5, -2)));
+        }
+
+        [Fact]
+        public void DividingTupleByScaler_ShouldDivideEachElementByScalar()
+        {
+            var myTuple = new RayTuple(1, -2, 3, -4);
+            RayTuple result = myTuple / 2;
+            Assert.True(result.Equals(new RayTuple(0.5, -1.0, 1.5, -2)));
+        }
+
+        [Fact]
+        public void ComputingMagnitudeOfVector1_ShouldBe1()
+        {
+            var myVector1 = new Vector(1, 0, 0);
+            var myVector2 = new Vector(0, 1, 0);
+            var myVector3 = new Vector(0, 0, 1);
+            Assert.Equal(1, myVector1.Magnitude());
+            Assert.Equal(1, myVector2.Magnitude());
+            Assert.Equal(1, myVector3.Magnitude());
+        } 
+
+        [Fact]
+        public void ComputingMagnitudeOfVector123_ShouldBeSqrt14()
+        {
+            var myVector = new Vector(1, 2, 3);
+            Assert.Equal(Math.Sqrt(14), myVector.Magnitude());
+        }
+
+        [Fact]
+        public void ComputingMagnitudeOfVectorNegative123_ShouldBeSqrt14()
+        {
+            var myVector = new Vector(-1, -2, -3);
+            Assert.Equal(Math.Sqrt(14), myVector.Magnitude());
+        }
+
+        [Fact]
+        public void NormalizingVector400_ShouldBe100()
+        {
+            var myVector = new Vector(4, 0, 0);
+            Assert.True(myVector.Normalize().Equals(new Vector(1, 0, 0)));
+        }
+
+        [Fact]
+        public void NormalizingComplexVector_ShouldWork()
+        {
+            var myVector = new Vector(1, 2, 3);
+            Assert.True(myVector.Normalize().Equals(new Vector(0.26726, 0.53452, 0.80178)));
+        }
+
+        [Fact]
+        public void MagnitudeOfNormalizedVector_ShouldBe1()
+        {
+            var myVector = new Vector(1, 2, 3);
+            Assert.Equal(1, myVector.Normalize().Magnitude());
+        }
+
+        [Fact]
+        public void DotProductOfTwoVectors_ShouldBeSumOfProducts()
+        {
+            var myVector1 = new Vector(1, 2, 3);
+            var myVector2 = new Vector(2, 3, 4);
+            Assert.Equal(20, myVector1.Dot(myVector2));
+        }
+
+        [Fact]
+        public void CrossProductOfTwoVectors_ShouldBeAnotherVector()
+        {
+            var myVector1 = new Vector(1, 2, 3);
+            var myVector2 = new Vector(2, 3, 4);
+            Assert.True(myVector1.Cross(myVector2).Equals(new Vector(-1, 2, -1)));
+            Assert.True(myVector2.Cross(myVector1).Equals(new Vector(1, -2, 1)));
+        }
     }
 }
--- RayTracer/Tuple.cs
+++ RayTracer/Tuple.cs
@@ -8,6 +8,49 @@ namespace RayTracer
 
         public RayTuple(double x, double y, double z, double w) : base(x, y, z, w) {}
 
+        public static RayTuple operator+(RayTuple a, RayTuple b)
+        {
+            if ( a.Item4 + b.Item4 > 1.0 )
+                throw new InvalidOperationException("Cannot add two points together.");
+
+            return new RayTuple(a.Item1 + b.Item1, 
+                                a.Item2 + b.Item2, 
+                                a.Item3 + b.Item3, 
+                                a.Item4 + b.Item4);
+        }
+
+        public static RayTuple operator-(RayTuple a)
+        {
+            return new RayTuple(-a.Item1, -a.Item2, -a.Item3, -a.Item4);
+        }
+
+        public static RayTuple operator-(RayTuple a, RayTuple b)
+        {
+            if ( a.Item4 - b.Item4 < 0.0 )
+                throw new InvalidOperationException("Cannot subtract a Point from a Vector.");
+
+            return new RayTuple(a.Item1 - b.Item1, 
+                                a.Item2 - b.Item2, 
+                                a.Item3 - b.Item3, 
+                                a.Item4 - b.Item4);
+        }
+
+        public static RayTuple operator*(RayTuple a, double multiplier)
+        {
+            return new RayTuple(a.Item1 * multiplier, 
+                                a.Item2 * multiplier, 
+                                a.Item3 * multiplier, 
+                                a.Item4 * multiplier);
+        }
+
+        public static RayTuple operator/(RayTuple a, double divisor)
+        {
+            return new RayTuple(a.Item1 / divisor, 
+                                a.Item2 / divisor, 
+                                a.Item3 / divisor, 
+                                a.Item4 / divisor);
+        }
+
         public override bool Equals(Object other)
         {
             RayTuple objTuple = other as RayTuple;
@@ -16,7 +59,10 @@ namespace RayTracer
                 return false;
             }
  
-            return (Math.Abs(objTuple.Item1 - this.Item1) < EPSILON) && (Math.Abs(objTuple.Item2 - this.Item2) < EPSILON) && (Math.Abs(objTuple.Item3 - this.Item3) < EPSILON) && (Math.Abs(objTuple.Item4 - this.Item4) < EPSILON);
+            return (Math.Abs(objTuple.Item1 - this.Item1) < EPSILON) && 
+                    (Math.Abs(objTuple.Item2 - this.Item2) < EPSILON) && 
+                    (Math.Abs(objTuple.Item3 - this.Item3) < EPSILON) && 
+                    (Math.Abs(objTuple.Item4 - this.Item4) < EPSILON);
         }
     }
 
@@ -31,6 +77,28 @@ namespace RayTracer
     {
         public Vector(double x, double y, double z) : base(x, y, z, 0.0)
         {
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(Item1*Item1 + Item2*Item2 + Item3*Item3);
+        }
+
+        public Vector Normalize()
+        {
+            return new Vector(Item1 / Magnitude(), Item2 / Magnitude(), Item3 / Magnitude());
+        }
+
+        public double Dot(Vector other)
+        {
+            return Item1 * other.Item1 + Item2 * other.Item2 + Item3 * other.Item3;
+        }
+
+        public Vector Cross(Vector other)
+        {
+            return new Vector(Item2 * other.Item3 - Item3 * other.Item2,
+                              Item3 * other.Item1 - Item1 * other.Item3,
+                              Item1 * other.Item2 - Item2 * other.Item1);
         }
     }
 }
