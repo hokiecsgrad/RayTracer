d4c570a70701c4440d41ad365c874f863e264bf2
Ryan Hagan <ryan@ryanhagan.net>
2021-07-31 10:16:11 -0400

    Reorganized main Program.cs

--- RayTracer.Program/Program.cs
+++ RayTracer.Program/Program.cs
@@ -1,38 +1,47 @@
 ﻿using System;
 using System.IO;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using RayTracer;
-
 
 namespace RayTracer.Program
 {
     class Program
     {
-        public static void Render()
+        static void Main(string[] args)
         {
-            var scene = new BumpMapScene();
-            var width = 800;
-            var height = 600;
-            var fov = 0.8;
-            var filename = "/Users/rhagan/VSCode Projects/RayTracer/RayTracer.Program/bumpmap.ppm";
-
             World world;
             Camera camera;
+            Canvas canvas;
 
+            string output = GetOutputImagePath();
+            int width = 800;
+            int height = 600;
+            double fov = 0.8;
+
+            (world, camera) = SetupWorld(width, height, fov);
+            canvas = Render(world, camera, new SteppedSampler(camera, 5));
+            //canvas = Render(world, camera, new DefaultSampler(camera));
+            //canvas = Render( world, camera, new AntiAliasSampler( camera, 8 ) );
+            //canvas = Render( world, camera, new FocalBlurSampler( camera, 1.0, 0.1, 8 ) );
+
+            SaveCanvas(canvas, output + "bumpmap.ppm");
+        }
+
+        public static (World, Camera) SetupWorld(int width, int height, double fov)
+        {
+            var scene = new BumpMapScene();
+            return scene.Setup(width, height, fov);
+        }
+
+        public static Canvas Render(World world, Camera camera, ISampler sampler)
+        {
             var sw = new Stopwatch();
             sw.Start();
-
-            (world, camera) = scene.Setup(width, height, fov);
-            Canvas canvas = camera.Render(world, new DefaultSampler(camera));
-            //Canvas canvas = camera.Render(world, new AntiAliasSampler(camera, 8));
-
-            Program.SaveCanvasToFile(canvas, filename);
-
+            Canvas canvas = camera.Render(world, sampler);
             sw.Stop();
 
-            var pixels = width*height;
+            var pixels = canvas.Width * canvas.Height;
             Console.WriteLine($"{sw.Elapsed}");
             Console.WriteLine($"{(double)pixels / sw.ElapsedMilliseconds}px/ms");
             Console.WriteLine($"Intersection tests: {Stats.Tests}");
@@ -40,10 +49,11 @@ namespace RayTracer.Program
             Console.WriteLine($"Secondary rays:     {Stats.SecondaryRays}");
             Console.WriteLine($"Shadow rays:        {Stats.ShadowRays}");
             //Console.WriteLine($"Super sampling:     {args.N}x");
-            //Console.WriteLine($"Output:             {Path.GetFullPath("out.ppm")}");
+
+            return canvas;
         }
 
-        private static void SaveCanvasToFile(Canvas canvas, string filename)
+        private static void SaveCanvas(Canvas canvas, string filename)
         {
             if (File.Exists(filename))
                 File.Delete(filename);
@@ -53,10 +63,29 @@ namespace RayTracer.Program
             writer.Close();
         }
 
-
-        static void Main(string[] args)
+        private static string GetOutputImagePath()
         {
-            Render();
+            StringBuilder imagePath = new StringBuilder();
+            string currpath = Directory.GetCurrentDirectory();
+
+            string[] pathParts = currpath.Split(
+                Path.DirectorySeparatorChar,
+                StringSplitOptions.RemoveEmptyEntries);
+
+            int index = 0;
+            foreach (string part in pathParts)
+            {
+                imagePath.Append(Path.DirectorySeparatorChar + pathParts[index++]);
+                if (part == "RayTracer.Program") break;
+            }
+            imagePath.Append(Path.DirectorySeparatorChar);
+            imagePath.Append("images");
+            imagePath.Append(Path.DirectorySeparatorChar);
+
+            if (!Directory.Exists(imagePath.ToString()))
+                Directory.CreateDirectory(imagePath.ToString());
+
+            return imagePath.ToString();
         }
     }
 }
