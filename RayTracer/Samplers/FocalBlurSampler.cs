b364d7c224ea38ae0d8c30d6a8850da1bc792c23
Ryan Hagan <ryan@ryanhagan.net>
2021-08-28 08:47:46 -0400

    Removed comments.

--- RayTracer/Samplers/FocalBlurSampler.cs
+++ RayTracer/Samplers/FocalBlurSampler.cs
@@ -35,22 +35,13 @@ namespace RayTracer
 
             for (int i = 0; i < this.numSamples; i++)
             {
-                // Get a vector with a random displacement 
-                // on a disk with radius 1.0 and use it to 
-                // offset the origin.
                 Vector apertureOffset = RandomInUnitDisk() * this.aperture;
 
-                // Create a new ray offset from the original ray
-                // and pointing at the focal point.
                 Point newOrigin = ray.Origin + apertureOffset;
                 Vector direction = (focalPoint - newOrigin).Normalize();
                 Ray secondaryRay = new Ray(newOrigin, direction, RayType.Primary);
 
-                // We probably should count these "secondary" rays
-                // as primary rays for stats purposes; this is consistent
-                // with RandomSuperSampler behavior.
                 Interlocked.Increment(ref Stats.PrimaryRays);
-
                 color += world.ColorAt(secondaryRay);
             }
 
@@ -59,18 +50,12 @@ namespace RayTracer
 
         public Ray RayForPixel(double px, double py)
         {
-            // the offset from the edge of the canvas to the pixel's center
             double xoffset = (px + 0.5) * this.Camera.PixelSize;
             double yoffset = (py + 0.5) * this.Camera.PixelSize;
 
-            // the untransformed coordinates of the pixel in world-space.
-            // (remember that the camera looks toward -z, so +x is to the *left*.)
             double world_x = this.Camera.HalfWidth - xoffset;
             double world_y = this.Camera.HalfHeight - yoffset;
 
-            // using the camera matrix, transform the canvas point and the origin,
-            // and then compute the ray's direction vector.
-            // (remember that the canvas is at z=-1)
             Point pixel = this.Camera.TransformInverse * new Point(world_x, world_y, -1);
             Point origin = this.Camera.TransformInverse * new Point(0, 0, 0);
             Vector direction = (pixel - origin).Normalize();
