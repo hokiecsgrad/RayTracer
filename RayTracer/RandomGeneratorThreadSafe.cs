e08b59c4e7ccf3f26fddb0cb72c007dde9cb4706
Ryan Hagan <ryan@ryanhagan.net>
2021-08-28 08:41:26 -0400

    Added a thread safe RNG

---
+++ RayTracer/RandomGeneratorThreadSafe.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace RayTracer
+{
+    public static class RandomGeneratorThreadSafe
+    {
+        private static Random _global = new Random();
+        [ThreadStatic]
+        private static Random _local;
+
+        public static double NextDouble()
+        {
+            Random inst = _local;
+            if (inst == null)
+            {
+                int seed;
+                lock (_global) seed = _global.Next();
+                _local = inst = new Random(seed);
+            }
+            return inst.NextDouble();
+        }
+    }
+}
